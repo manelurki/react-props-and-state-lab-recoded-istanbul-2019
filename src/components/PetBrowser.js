aaaf4c7b97388d1065aca7e11cf0002af3e49230
manel turki <menelturki@gmail.com>
2020-03-24 10:21:40 +0000

    Automatically backed up by Learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,31 +1,13 @@
 import React from 'react'
 
-class Pet extends React.Component {
+import Pet from './Pet'
+
+class PetBrowser extends React.Component {
   render() {
-    return (
-      <div className="card">
-        <div className="content">
-          <a className="header">
-            {/*'♀' OR '♂' */}
-            {this.props.pet.name}
-            {this.props.pet.gender === 'female' ? '♀' : '♂'}
-          </a>
-          <div className="meta">
-            <span className="date">{this.props.pet.type}</span>
-          </div>
-          <div className="description">
-            <p>Age: {this.props.pet.age}</p>
-            <p>Weight: {this.props.pet.weight}</p>
-          </div>
-        </div>
-        {
-            this.props.pet.isAdopted ?  
-            <button className="ui disabled button" >Already adopted</button> :
-            <button className="ui primary button" onClick={()=>this.props.onAdoptPet(this.props.pet.id)}>Adopt pet</button>
-          }
-      </div>
-    )
+    return <div className="ui cards">
+      {this.props.pets.map(pet => <Pet pet={pet} onAdoptPet={this.props.onAdoptPet}/>)}
+    </div>
   }
 }
 
-export default Pet
+export default PetBrowser
